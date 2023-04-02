b1d199ae53b56fadc3477864452a969d61a12626
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-04-02 18:36:20 -0300

    fix: fixing tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Go Report Card](https://img.shields.io/badge/go%20report-A+-brightgreen.svg?style=flat)](#go-report-card)
+
 # go-slice-hofs
 This Golang package provides higher-order functions that operate on slices, enabling the user to manipulate slice data without the need for explicit iteration. These functions serve as convenient abstractions, simplifying common operations like filtering, mapping, reducing, and transforming slice elements.
--- hofs/hofs_test.go
+++ hofs/hofs_test.go
@@ -98,7 +98,6 @@ func TestHofs(t *testing.T) {
 		if !reflect.DeepEqual(result2, test30OrOlder.expected) {
 			t.Errorf("Filter failed: received %+v, expected %+v", result2, test30OrOlder.expected)
 		}
-		}
 	})
 
 	t.Run("Tests the hofs.Every function", func(t *testing.T) {
@@ -128,7 +127,6 @@ func TestHofs(t *testing.T) {
 		}
 		if result2 != testIfEveryoneIsOver18.expected {
 			t.Errorf("Every failed: received %+v, expected %+v", result2, testIfEveryoneIsOver18.expected)
-		}
 		}
 	})
 
