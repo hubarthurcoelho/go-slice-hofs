a83ec7cf5087c52fbfdd8df21d8c04a07301e247
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-04-02 17:26:48 -0300

    feat: adding description

--- hofs/map.go
+++ hofs/map.go
@@ -1,7 +1,8 @@
 package hofs
 
-func Map[T any, Y any](slice []Y, f func(arg Y) T) []T {
-	result := make([]T, 0, len(slice))
+// Map will receive a slice of T values and return a slice of Y values of the same length.
+func Map[T any, Y any](slice []T, f func(arg T) Y) []Y {
+	result := make([]Y, 0, len(slice))
 	for _, item := range slice {
 		newItem := f(item)
 		result = append(result, newItem)
